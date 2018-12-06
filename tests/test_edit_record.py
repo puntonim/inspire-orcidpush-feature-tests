54a6cbf6148dfda0c404f0adc492cf65fd79182e
turtle321 <>
2018-12-06 11:51:00 +0100

    Add recaching_all_putcodes test

--- tests/test_edit_record.py
+++ tests/test_edit_record.py
@@ -31,11 +31,35 @@ def test_orcid_login():
     browser.quit()
 
 
-def test_edit_record_and_push_to_orcid():
+def test_edit_record_and_push_new_work_to_orcid():
     # Clean up the orcid work.
     orcid_api_client.delete_work(test_data.ORCID, test_data.TOKEN, test_data.RECID)
     orcid_api_client.assert_no_putcode_for_work(test_data.ORCID, test_data.TOKEN, test_data.RECID)
 
+    # Edit the record.
+    browser = Chrome()
+    inspire_ui_client.login_as_admin(browser)
+    browser.visit('{}/{}/{}'.format(test_data.BASEURL, test_data.RECORD_EDITOR_ENDPOINT, test_data.RECID))
+    save = browser.find_visible_by_xpath(test_data.SAVE_XPATH)[0]
+    save.click()
+
+    # Ensure (using Flower) the Celery task has been successfully executed.
+    flower_api_client.assert_orcid_push_task_successful(test_data.ORCID, test_data.RECID, 60*5)
+
+    # Ensure the work is visible in orcid.
+    orcid_api_client.assert_any_putcode_for_work(test_data.ORCID, test_data.TOKEN, test_data.RECID)
+    browser.quit()
+
+
+def test_edit_record_and_put_updated_work_to_orcid_with_recaching_all_putcodes():
+    # Note: this test assumes the work with recid test_data.RECID to have been
+    # already pushed to orcid. This happens if the test
+    # `test_edit_record_and_push_new_work_to_orcid` has run already.
+
+    # Ensure the work is visible in orcid.
+    orcid_api_client.assert_any_putcode_for_work(test_data.ORCID, test_data.TOKEN, test_data.RECID)
+
+    # Edit the record.
     browser = Chrome()
     inspire_ui_client.login_as_admin(browser)
     browser.visit('{}/{}/{}'.format(test_data.BASEURL, test_data.RECORD_EDITOR_ENDPOINT, test_data.RECID))
